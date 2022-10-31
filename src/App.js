419647924ad623752240fd1a90128f9cea3ebd42
RowDog <mrdiamonddirt@gmail.com>
2022-10-31 17:15:57 +0000

    cross ref forces api

--- src/App.js
+++ src/App.js
@@ -29,25 +29,38 @@ const geoUrl =
   "https://raw.githubusercontent.com/deldersveld/topojson/master/countries/united-kingdom/uk-counties.json";
 
 const MapChart = () => {
-  function onclick(geo) {
+  async function onclick(geo) {
     console.log(geo);
     console.log(geo.properties.NAME_2);
     updateLocation(geo.properties.NAME_2);
     // openModal()
-    returnPoliceForce()
+    returnPoliceForce(geo.properties.NAME_2)
+    try {
+      const response = await fetch(`https://data.police.uk/api/forces/${geo.properties.NAME_2.toLowerCase().split(' ').join('-')}`)
+    const data = await response.json()
+    // console.log(data)
+    setPoliceInfo(data)
+    } catch (error) {
+      // setPoliceInfo(error.msg)
+      console.log(error)
+    }
+    
   }
 
   let subtitle;
   const [modalIsOpen, setIsOpen] = React.useState(false);
   const [location, updateLocation] = useState("");
   const [policedata, setPoliceData] = useState([])
+  const [policeInfo, setPoliceInfo] = useState({})
 
   function openModal() {
     setIsOpen(true);
   }
 
-  function returnPoliceForce() {
-    let locationLC = location.toLowerCase();
+ const [selectedCounty, updateCounty] = useState("")
+ 
+  function returnPoliceForce(currentloc) {
+    let locationLC = currentloc.toLowerCase();
     for (let i = 0; i <  policedata.length; i++) {
       // console.log(policedata[i])
       if (locationLC === policedata[i].id) {
@@ -55,11 +68,15 @@ const MapChart = () => {
         console.log(`returned ${locationLC}`)
         console.log(`returned police data id = ${policedata[i].id}`)
         console.log(`returned police data id = ${policedata[i].name}`)
-
+        updateCounty(policedata[i].name)
+        console.log(`Selected County ${selectedCounty}`)
+        return
       } else {
         console.log('no match')
         console.log(`returned ${locationLC}`)
         console.log(`returned police data id = ${policedata[i].id}`)
+        updateCounty("unable to find")
+
       }
       
     }
@@ -74,8 +91,11 @@ const MapChart = () => {
     setIsOpen(false);
   }
 
+  function name(params) {
+    
+  }
+
   useEffect(() => {
-    returnPoliceForce()
     const fetchPoliceData = async () => {
       try {
         const response = await fetch(
@@ -97,8 +117,9 @@ const MapChart = () => {
  
 
   return (
+    <>
     <div>
-      <button onClick={openModal}>Open model</button>
+      <button className="infoBtn" onClick={openModal}>Info</button>
       <Modal
         isOpen={modalIsOpen}
         // onAfterOpen={afterOpenModal}
@@ -106,9 +127,10 @@ const MapChart = () => {
         style={customStyles}
         contentLabel="Example Modal">
         <h2 className="modelheader" >County Info</h2>
-        <p className={'modalloc'}>{location}</p>
-        {/* <p className={'modalloc2'}>{policedata}</p> */}
-      </Modal>
+        <p key="modalloc" className={'modalloc'}>{location}</p>
+        <p>{policeInfo.url}</p>
+        <p>{policeInfo.name}</p></Modal>
+
       <ComposableMap projection="geoMercator">
         <ZoomableGroup center={[-3, 55]} zoom={10}>
           <Geographies geography={geoUrl} fill="rgba(0, 139, 1, 1)">
@@ -117,12 +139,28 @@ const MapChart = () => {
                 <Geography
                   key={geo.rsmKey}
                   geography={geo}
+                  style={{
+                    default: {
+                      fill: "green",
+                      stroke: "black",
+                      strokeWidth: "0.1px"
+                    },
+                    hover: {
+                      fill: "#F53",
+                    },
+                    pressed: {
+                      fill: "#E42",
+                      stroke: "red",
+                      strokeWidth: "0.1px"
+                    },
+                  }}
                   onMouseEnter={() => {
-                  
                   }}
                   onMouseLeave={() => {
                   }}
                   onClick={() => onclick(geo)}
+                  fill="orange"
+                  // stroke="black"
                 />
               ))
             }
@@ -136,6 +174,16 @@ const MapChart = () => {
         </ZoomableGroup>
       </ComposableMap>
     </div>
+    <div>
+      <h5>Info</h5>
+      <p key={'loc'}>{location}</p>
+      {/* <p>{selectedCounty}</p> */}
+      <div className="description">
+      {/* {policeInfo.description} */}
+      {policeInfo.url}
+      </div>
+    </div>
+    </>
   );
 };
 
